c7d5d732d0b5bdfedfbfb2b7a9f3891fc53bcf9c
Aymen <mn05052002@gmail.com>
2022-11-07 20:19:25 +0500

    :(

--- mmessage/Main.java
+++ mmessage/Main.java
@@ -9,6 +9,7 @@ msgdata[3]=new Mmessage("maryam","ibtasam","how do you do","2_7_2221");
 msgdata[4]=new Mmessage("fairy","imbisat","I'm Good","2_7_2221");
 
 String mesg ="Good";
+//contains functions
 
 for (int i=0;i<5;i++)
 {
@@ -17,10 +18,12 @@ if(msgdata[i].message.contains(mesg))
 System.out.println(msgdata[i].Get());
 }
 }
+//compare to 
 String Sender="maryam";
+
 for (int i=0;i<5;i++)
 {
-if(msgdata[i].sender==Sender)
+if(msgdata[i].sender.compareTo(Sender)==0)
 {
 System.out.println(msgdata[i].Get());
 }
@@ -28,19 +31,24 @@ System.out.println(msgdata[i].Get());
 String rec="zoha";
 for (int i=0;i<5;i++)
 {
-if(msgdata[i].sendto==rec)
+if(msgdata[i].sender.compareTo(rec)==0)
 {
 System.out.println(msgdata[i].Get());
 }
 }
+//length function
 System.out.println(rec.length());
- 
+ //charAt function
 for (int i=0;i<rec.length();i++)
 {
 
 System.out.println(rec.charAt(i));
 
 }
+//substring
+String sentence="My name is Aymen Noor";
+System.out.println(sentence.substring(11));
+
 }  
  
 
